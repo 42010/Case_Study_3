5898281802ecbdf3957f6dc4c3e98bdf9a57993c
42010 <72303709+42010@users.noreply.github.com>
2021-09-12 15:38:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function identify_1(){
+    document.getElementById("image_identify_1").innerHTML = "Output on Microsoft azure : Bottle";
+    document.getElementById("image_identify_1.1").innerHTML = "Output on Mobile net : Water bottle";
+    document.getElementById("result_image_1").innerHTML = "Result - Mobile Net is more accurate!";
+}
+
+function identify_2(){
+    document.getElementById("image_identify_2").innerHTML = "Output on Microsoft Azure : Headphones";
+    document.getElementById("image_identify_2.1").innerHTML = "Output on Mobile Net : Earphones, Airpod";
+    document.getElementById("result_image_2").innerHTML = "Result - Microsoft Azure is more accurate!";
+}
+
+function identify_3(){
+    document.getElementById("image_identify_3").innerHTML = "Output on Microsoft Azure : Lamborghini Veneno";
+    document.getElementById("image_identify_3.1").innerHTML = "Output on Mobile Net : Car, super car";
+    document.getElementById("result_image_3").innerHTML = "Result - Microsoft azure is more accurate";
+}
+
+function What_to_use(){
+    document.getElementById("what_to_use").innerHTML = "Use Microsoft Azure (prefered)";
+}
+
+function result_get(){
+    document.getElementById("result_get1").innerHTML = "Score : Microsoft Azure got 2/3 correct and Mobile Net got 1/3 correct";
+}
